7020676c5387a612f0de2c440c70e2572307558b
Arne Krüger <kruegerarneak@gmail.com>
2018-10-03 13:58:57 +0200

    first commit

--- run_once/000_install_system.sh
+++ run_once/000_install_system.sh
@@ -3,34 +3,34 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 
-apt update
-apt-get -y install curl htop apt-transport-https
+echo $MY_PASSWORD | sudo -S apt-get update
+echo $MY_PASSWORD | sudo -S apt-get -y install curl htop apt-transport-https
 
 
 
 
 #install influxdb
-curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-curl https://packagecloud.io/gpg.key | sudo apt-key add -
+echo $MY_PASSWORD | sudo -S curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+echo $MY_PASSWORD | sudo -S curl https://packagecloud.io/gpg.key | sudo apt-key add -
 
-echo "deb https://repos.influxdata.com/debian stretch stable" | tee /etc/apt/sources.list.d/influxdb.list
-echo "deb https://packagecloud.io/grafana/stable/debian/ stretch main" | tee /etc/apt/sources.list.d/grafana.list
+echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+echo "deb https://packagecloud.io/grafana/stable/debian/ stretch main" | sudo tee /etc/apt/sources.list.d/grafana.list
 
 #install python
 
-apt update
-apt-get -y install influxdb python-pip python-influxdb python-protobuf grafana
-cp -rf $SCRIPTPATH/../etc/influxdb/influxdb.conf /etc/influxdb/influxdb.conf
-cp -rf $SCRIPTPATH/../etc/grafana/grafana.ini /etc/grafana/grafana.ini
+echo $MY_PASSWORD | sudo -S apt update
+echo $MY_PASSWORD | sudo -S apt-get -y install influxdb python-pip python-influxdb python-protobuf grafana
+echo $MY_PASSWORD | sudo -S cp -rf $SCRIPTPATH/../etc/influxdb/influxdb.conf /etc/influxdb/influxdb.conf
+echo $MY_PASSWORD | sudo -S cp -rf $SCRIPTPATH/../etc/grafana/grafana.ini /etc/grafana/grafana.ini
 
-pip install paho-mqtt
+echo $MY_PASSWORD | sudo -S pip install paho-mqtt
 
 mkdir /home/enerlyzer_receiver/influx_data
 chown /home/enerlyzer_receiver/influx_data
 
 
-systemctl daemon-reload
-systemctl start grafana-server
-service influxdb start
-systemctl enable grafana-server.service
-systemctl enable influxdb
+echo $MY_PASSWORD | sudo -S systemctl daemon-reload
+echo $MY_PASSWORD | sudo -S systemctl start grafana-server
+echo $MY_PASSWORD | sudo -S service influxdb start
+echo $MY_PASSWORD | sudo -S systemctl enable grafana-server.service
+echo $MY_PASSWORD | sudo -S systemctl enable influxdb
